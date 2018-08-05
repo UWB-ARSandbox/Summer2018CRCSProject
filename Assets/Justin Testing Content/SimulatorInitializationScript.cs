cc59bb7fc74bfa58f5c81cee6e21f3c4e5a8b8e7
Justin Baker <aluc4rd@uw.edu>
2018-08-05 14:16:12 -0700

    Making Scrit for simulator intitilization.

--- Assets/Justin Testing Content/SimulatorInitializationScript.cs
+++ Assets/Justin Testing Content/SimulatorInitializationScript.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimulatorInitializationScript : MonoBehaviour {
+public class SimulatorInitializationScript : LocalEventHandler {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected override void OnLocalEvent(object sender, ASLLocalEventManager.LocalEventArgs args)
+    {
+        switch (args.MyEvent)
+        {
+            case ASLLocalEventManager.LocalEvents.PlayerInitialized:
+                {
+                    break;
+                }
+            case ASLLocalEventManager.LocalEvents.SimulatorCameraRigInstantiated:
+                {
+                    OnSimulatorInstantiated();
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+    void OnSimulatorInstantiated()
+    {
+        GameObject.Find("VRTK_SDKManager").GetComponent<VRTK.VRTK_SDKManager>().enabled = true;
+    }
 }
