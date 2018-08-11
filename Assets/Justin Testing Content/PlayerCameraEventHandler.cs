ff4d3fa8d65bb26b9ab4b9983d205910330d1463
Justin Baker <JustinRBaker88@gmail.com>
2018-08-10 18:26:45 -0700

    Created event for setting the proper camera tag.

--- Assets/Justin Testing Content/PlayerCameraEventHandler.cs
+++ Assets/Justin Testing Content/PlayerCameraEventHandler.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCameraEventHandler : MonoBehaviour {
+public class PlayerCameraEventHandler : LocalEventHandler
+{
+    public Camera playerCamera;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected override void OnLocalEvent(object sender, ASLLocalEventManager.LocalEventArgs args)
+    {
+        switch (args.MyEvent)
+        {
+            case ASLLocalEventManager.LocalEvents.PlayerInstanceActive:
+                {
+                    break;
+                }
+            default:
+                {
+                    Debug.Log("Event not handled");
+                    break;
+                }
+        }
+    }
+
+    private void SetPrimaryCameraTag()
+    {
+        if (playerCamera == null)
+        {
+            FindCamera();
+        }
+        playerCamera.gameObject.tag = "Local Primary Camera";
+    }
+
+    private void FindCamera()
+    {
+        playerCamera = transform.GetComponentInChildren<Camera>(true);
+    }
 }
